deb078b762e9d917ac2403a086c02135809ab2d2
jpbertti <joaobertty@gmail.com>
2024-11-18 11:53:47 -0300

    Adicionar arquivos de projeto.

---
+++ ex1_vetor/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ex1_vetor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsbAleatorios = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lsbOrdem = new System.Windows.Forms.ListBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(544, 139);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(95, 35);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gere 10 Valores Aleatórios";
+            // 
+            // lsbAleatorios
+            // 
+            this.lsbAleatorios.ForeColor = System.Drawing.Color.IndianRed;
+            this.lsbAleatorios.FormattingEnabled = true;
+            this.lsbAleatorios.Location = new System.Drawing.Point(29, 66);
+            this.lsbAleatorios.Name = "lsbAleatorios";
+            this.lsbAleatorios.Size = new System.Drawing.Size(222, 108);
+            this.lsbAleatorios.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(287, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(244, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Colocar em Ordem Crescente";
+            // 
+            // lsbOrdem
+            // 
+            this.lsbOrdem.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lsbOrdem.FormattingEnabled = true;
+            this.lsbOrdem.Location = new System.Drawing.Point(291, 66);
+            this.lsbOrdem.Name = "lsbOrdem";
+            this.lsbOrdem.Size = new System.Drawing.Size(222, 108);
+            this.lsbOrdem.TabIndex = 6;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(544, 66);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(95, 35);
+            this.btnMostrar.TabIndex = 7;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 192);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lsbOrdem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lsbAleatorios);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lsbAleatorios;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lsbOrdem;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ ex1_vetor/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex1_vetor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lsbAleatorios.Items.Clear();
+            lsbOrdem.Items.Clear();
+
+            int[] numeros = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i <10 ; i++)
+            {
+                numeros[i] = random.Next(1, 100);
+                lsbAleatorios.Items.Add(numeros[i]);
+            }
+
+            Array.Sort(numeros);
+
+            foreach (var numero in numeros)
+            {
+                lsbOrdem.Items.Add(numero);
+            }
+        }
+
+
+
+    }
+}
